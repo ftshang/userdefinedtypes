2960d65d0b3483921db15ebb5a2fc3e69f2077eb
ftshang <74879078+ftshang@users.noreply.github.com>
2022-03-16 02:31:04 +0000

    added nested user defined types and implemented functions

--- .ccls-cache/@home@runner@userdefinedtypes/main.cpp
+++ .ccls-cache/@home@runner@userdefinedtypes/main.cpp
@@ -7,17 +7,47 @@ int height;
 float hairLength;
 float GPA;
 unsigned int SATScore;
+int distanceTraveled = 0;
 
 void run(int howFast, bool startWithLeftFoot);
-void sprint(int speed, bool startWithLeftFoot);
+
+struct Limb {
+    void stepForward();
+    int stepSize();
+};
 
 };
+
+void Person::Limb::stepForward() {
+    std::cout << "You moved forward!" << std::endl;
+}
+
+int Person::Limb::stepSize() {
+    return 10;
+}
+
+void Person::run(int howFast, bool startWithLeftFoot) 
+{
+    Limb leftFoot, rightFoot;
+    if (startWithLeftFoot == true) {
+        leftFoot.stepForward();
+        rightFoot.stepForward();
+    }
+    else {
+        rightFoot.stepForward();
+        leftFoot.stepForward();
+    }
+    distanceTraveled += rightFoot.stepSize();
+    distanceTraveled += leftFoot.stepSize();
+}
 
 
 int main() {
     Person p, person, amanda;
-    std::cout << p.age << std::endl;
-    std::cout << amanda.age << std::endl;
+    for (int i = 0; i < 5; i++) {
+        p.run(14, false);
+    }
+    std::cout << p.distanceTraveled << std::endl;
 
     return 0;
 }
